958bcd9953d8de22d08236dd9f8bf41b40d1ad72
kamilsaeed <kamilsaeedkamali18@gmail.com>
2026-04-22 18:27:06 +0500

    Removed stray URL typo

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,3 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
-
-http://googleusercontent.com/immersive_entry_chip/0
-http://googleusercontent.com/immersive_entry_chip/1
